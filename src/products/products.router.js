18f5bb98d3e476cb181b4a8ea2920d68a82b4066
Molly <mojonesycreative@gmail.com>
2022-05-30 23:14:31 -0400

    added regex to ensure at least one digit in id

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -3,6 +3,7 @@ const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:productId").get(controller.read).all(methodNotAllowed);
+router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
+// Regex ensures that the productId param contains one or more digits.
 
 module.exports = router;
